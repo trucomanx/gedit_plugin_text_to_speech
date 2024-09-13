e6f972e1811cd94dc252ec5dffa131ed0b114b09
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-09-12 22:25:49 -0300

    ordenando

--- plugins/text_to_speech/__init__.py
+++ plugins/text_to_speech/__init__.py
@@ -165,7 +165,7 @@ class ExampleWindowActivatable(GObject.Object, Gedit.WindowActivatable, PeasGtk.
         
         info=ler_json_como_dict("text_to_speech.json");
         server_url='http://localhost:5000';
-        Dict={ "text": selected_text, "language": info["language"], "split_pattern": ["."], "speed":1.25 };
+        Dict={ "text": selected_text, "language": info["language"], "split_pattern": [". \n",".\n","\n\n"], "speed":1.25 };
         
         #print(selected_text)
         #print(Dict)
